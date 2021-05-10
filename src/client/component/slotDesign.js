a758f640c0335e7dab4cfbf1fde477a3bdbe4e76
vipul10-git <60420229+vipul10-git@users.noreply.github.com>
2021-05-10 18:34:02 +0530

    Update slotDesign.js

--- src/client/component/slotDesign.js
+++ src/client/component/slotDesign.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const SlotDesign = () =>{
+const SlotDesign = (props) =>{
     let gamePad= [];
     for(let i=0;i<8;i++){
         for(let j=0;j<8;j++){
             gamePad.push(
                 <div className="outerCircle" key={`${i}${j}`}>
-                    <div className="innerCircle" id={`col-${i}--row-${j}`} data-col={i} data-row={j} />
+                    <div className="innerCircle" onMouseOver={props.onMouseOver} onMouseLeave={props.onMouseLeave} data-col={i} data-row={j}   id={`col-${i}--row-${j}`} >
+                        <div className='slotImgAad' data-col={i} data-row={j} ></div>
+                    </div>
                 </div>
             );
         }
